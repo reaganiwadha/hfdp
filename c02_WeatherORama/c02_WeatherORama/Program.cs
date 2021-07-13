1089f3a114f443e6ed06ee457ee6e74b7aeaa7e0
reaganiwadha <reaganiwadha@protonmail.com>
2021-07-13 22:02:25 +0700

    added weather orama base

---
+++ c02_WeatherORama/c02_WeatherORama/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace c02_WeatherORama
+{
+    // use C#'s events    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
