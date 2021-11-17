3fef3d80b313f40c10414e6bf4fa7c13277501cd
reaganiwadha <reaganiwadha@protonmail.com>
2021-11-17 17:37:31 +0700

    added singleton chocolateFactory

---
+++ c05_ChocolateFactory/ChocolateFactory.cs
@@ -0,0 +1,41 @@
+﻿namespace c05_ChocolateFactory
+{
+    public class ChocolateFactory
+    {
+        private static ChocolateFactory _instance;
+
+        private bool _empty;
+        private bool _boiled;
+
+        private ChocolateFactory()
+        {
+            _empty = true;
+            _boiled = false;
+        }
+
+        public void Drain()
+        {
+            if (!_empty && _boiled) _empty = true;
+        }
+
+        public void Boil()
+        {
+            if (!_empty && !_boiled) _boiled = true;
+        }
+
+        public void Fill()
+        {
+            if (!_empty) return;
+            _empty = false;
+            _boiled = false;
+        }
+
+        public bool IsEmpty() => _empty;
+        public bool IsBoiled() => _boiled;
+
+        public static ChocolateFactory GetInstance()
+        {
+            return _instance ??= new ChocolateFactory();
+        }
+    }
+}
---
+++ c05_ChocolateFactory/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace c05_ChocolateFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chocolateFactory = ChocolateFactory.GetInstance();
+            chocolateFactory.Fill();
+            chocolateFactory.Boil();
+            
+            Console.WriteLine($"Is chocolateFactory boiled? {chocolateFactory.IsBoiled()}");
+            
+            chocolateFactory.Drain();
+        }
+    }
+}
